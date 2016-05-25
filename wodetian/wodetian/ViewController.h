fdd6099a45b8fd157b3b41af1fef1a1df2e0bba7
lanou <1565645734@qq.com>
2016-05-25 16:14:09 +0800

    lkfjkaljf;lsdjkfl;

---
+++ wodetian/wodetian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wodetian
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 唐甲朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wodetian/wodetian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wodetian
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 唐甲朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
